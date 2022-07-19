5446711fac7ca484c7a7e0311c155c1cd4e330b8
IsraaHaseeba <haseebaisraa@gmail.com>
2022-07-19 13:25:15 +0300

    api

---
+++ UsersAPI/Controllers/UsersController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UserAPI.Models;
+using UsersAPI.Repo;
+
+namespace UserAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<User>> GetAll()
+        {
+            return UserRepo.getAll();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<User> Get(int id)
+        {
+            var user = UserRepo.Get(id);
+            if (user == null) return NotFound();
+            return user;
+        }
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var user = UserRepo.Get(id);
+            if (user == null) return NotFound();
+            UserRepo.Delete(id);
+            return Ok();
+        }
+
+        [HttpPost]
+        public ActionResult Create(User user)
+        {
+
+            var _user = UserRepo.Get(user.Id);
+            if (_user != null) return BadRequest("User already exists.");
+            UserRepo.Add(user);
+            return Ok();
+        }
+
+        [HttpPut]
+        public ActionResult Update(int id, User user)
+        {
+            var _user = UserRepo.Get(id);
+            if (_user == null)
+                return NotFound();
+            if(user.Id != id) return BadRequest("Id cannot be updated!");
+            UserRepo.update(user);
+            return Ok();
+        }
+    }
+}
---
+++ UsersAPI/Repo/UserRepo.cs
@@ -0,0 +1,56 @@
+﻿using UserAPI.Models;
+
+namespace UsersAPI.Repo
+{
+    public class UserRepo
+    {
+        static List<User> users { get; set; }
+
+        static UserRepo()
+        {
+            users = new List<User>()
+            {
+                new User(){ Id = 1, Name = "Mohammad", Email = "mohammad12@gmail.com"},
+                new User(){ Id = 2, Name = "Luay", Email = "Luay12@gmail.com"},
+                new User(){ Id = 3, Name = "Huda", Email = "huda12@gmail.com"},
+                new User(){ Id = 4, Name = "Lama", Email = "lama12@gmail.com"},
+                new User(){ Id = 5, Name = "Roa", Email = "roa12@gmail.com"}
+
+
+
+
+            };
+        }
+
+        public static List<User> getAll()
+        {
+            return users;
+        }
+
+        public static User Get(int id)
+        {
+            return users.FirstOrDefault(user => user.Id == id);
+        }
+
+        public static void Delete(int id)
+        {
+            var user = Get(id);
+            if (user != null)
+                users.Remove(user);
+        }
+
+        public static void Add(User user)
+        {
+            users.Add(user);
+        }
+
+        public static void update(User user)
+        {
+            var index = users.FindIndex(e => e.Id == user.Id);
+            if (index == -1)
+                return;
+            users[index] = user;
+        }
+    }
+
+}
